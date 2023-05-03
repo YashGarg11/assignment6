0374249fd714f9a1a057004d93d3381212bdc720
YashGarg11 <131020283+YashGarg11@users.noreply.github.com>
2023-05-04 00:45:26 +1100

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
-const userInput = document.getElementById("user-input");
-const submitBtn = document.getElementById("submit-btn");
-const result = document.getElementById("result");
+let computerGuesses = 5; // number of guesses the computer has
+let computerNumber; // the number the computer is trying to guess
+let userNumber; // the number entered by the user
+let result = document.getElementById("result"); // the element to display the game result
 
-let numAttempts = 0;
-let guess = Math.floor(Math.random() * 100) + 1;
-
-function checkInput() {
-  if (userInput.value < 1 || userInput.value > 100) {
-    result.innerHTML = "Invalid input. Please enter a number between 1 and 100.";
-    return false;
-  }
-  return true;
-}
-
-function resetGame() {
-  numAttempts = 0;
-  guess = Math.floor(Math.random() * 100) + 1;
-}
-
-function playGame() {
-  if (checkInput()) {
-    numAttempts++;
-
-    if (numAttempts > 5) {
-      result.innerHTML = `Sorry, the computer was unable to guess your number. You win!`;
-      resetGame();
-      return;
-    }
-
-    if (userInput.value == guess) {
-      result.innerHTML = `The computer guessed your number (${guess}) in ${numAttempts} attempts. The computer wins!`;
-      resetGame();
-      return;
-    }
-
-    if (userInput.value < guess) {
-      result.innerHTML = `The computer guessed ${userInput.value}. The number is higher.`;
+// function to start the game
+function startGame() {
+    userNumber = parseInt(document.getElementById("user-input").value);
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 100) {
+        result.innerHTML = "Invalid number! Please enter a number between 1 and 100.";
     } else {
-      result.innerHTML = `The computer guessed ${userInput.value}. The number is lower.`;
+        result.innerHTML = "";
+        computerNumber = Math.floor(Math.random() * 100) + 1; // generate a random number between 1 and 100
+        computerGuess();
     }
-
-    userInput.value = "";
-  }
 }
 
-submitBtn.addEventListener("click", playGame);
+// function to make a computer guess
+function computerGuess() {
+    if (computerGuesses === 0) {
+        result.innerHTML = `Sorry, the computer was unable to guess your number ${userNumber}. You win!`;
+    } else if (computerNumber === userNumber) {
+        result.innerHTML = `The computer guessed your number ${userNumber}! The computer wins!`;
+    } else {
+      let guess = Math.floor(Math.random() * 100) + 1; // generate a random guess
+      computerGuesses--;
+      if (guess === userNumber) {
+          result.innerHTML = `The computer guessed your number ${userNumber}! The computer wins!`;
+      } else {
+          result.innerHTML = `The computer guessed ${guess}.`;
+          setTimeout(computerGuess, 1000); // delay the next guess by 1 second
+      }
+  }
+}
